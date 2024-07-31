45b7e199f9a5a666d28743ab58ce352a70242703
Romast27 <108524027+Romast27@users.noreply.github.com>
2024-07-31 10:56:12 +0300

    Update settings.py
    delete config information

--- ScanCheck/settings.py
+++ ScanCheck/settings.py
@@ -19,15 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l8pm2^nos6@4_^ab*-19%6el!heafuee-p#$c$*wcwey6*_o79'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -75,14 +66,7 @@ WSGI_APPLICATION = 'ScanCheck.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DB_ScanCheck',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
+    'default': 1
 }
 
 
